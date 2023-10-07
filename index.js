88ee0e1921cbd4870b001f80765bcd5dbba4c959
lealobanov <lea.lobanov@icloud.com>
2023-10-07 23:17:03 +1300

    Onboarding content

---
+++ index.js
@@ -0,0 +1,26 @@
+// Pass the repo name
+const recipe = "metadata-views";
+
+//Generate paths of each code file to render
+const contractPath = `${recipe}/cadence/contract.cdc`;
+const transactionPath = `${recipe}/cadence/transaction.cdc`;
+
+//Generate paths of each explanation file to render
+const smartContractExplanationPath = `${recipe}/explanations/contract.txt`;
+const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
+
+export const metadataViews = {
+  slug: recipe,
+  title: "Metadata Views",
+  description: "",
+  createdAt: Date(2022, 9, 14),
+  author: "Flow Blockchain",
+  playgroundLink:
+    "https://play.onflow.org/41befd2d-31f3-47f0-ae30-aad776961e31?type=script&id=60b68d62-f15e-402e-9048-ef3f27e224af",
+  excerpt:
+    "This is how you use the metadata contract to actually access the views to show your metadata.",
+  smartContractCode: contractPath,
+  smartContractExplanation: smartContractExplanationPath,
+  transactionCode: transactionPath,
+  transactionExplanation: transactionExplanationPath,
+};
