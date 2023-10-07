7fec17d6a6fd830325d6d582e852b912bcdc7f46
lealobanov <lea.lobanov@icloud.com>
2023-10-07 23:24:20 +1300

    Update creation date

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ export const metadataViews = {
   slug: recipe,
   title: "Metadata Views",
   description: "",
-  createdAt: Date(2022, 9, 14),
+  createdAt: Date(2022, 3, 1),
   author: "Flow Blockchain",
   playgroundLink:
     "https://play.onflow.org/41befd2d-31f3-47f0-ae30-aad776961e31?type=script&id=60b68d62-f15e-402e-9048-ef3f27e224af",
