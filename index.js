c428eee4a435faed0f43420440318d599baa446f
lealobanov <lea.lobanov@icloud.com>
2023-10-13 20:05:40 +0900

    Removing description

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
 export const metadataViews = {
   slug: recipe,
   title: "Metadata Views",
-  description: "",
   createdAt: Date(2022, 3, 1),
   author: "Flow Blockchain",
   playgroundLink:
