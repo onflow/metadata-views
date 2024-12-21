c968a74f775a527000f84155c7bdd2aea1d2d78e
Lea Lobanov <lea.lobanov@icloud.com>
2024-12-21 14:29:57 +0400

    Simplify recipe fields

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 // Pass the repo name
 const recipe = "metadata-views";
 
-//Generate paths of each code file to render
-const contractPath = `${recipe}/cadence/contract.cdc`;
-const transactionPath = `${recipe}/cadence/transaction.cdc`;
-
-//Generate paths of each explanation file to render
-const smartContractExplanationPath = `${recipe}/explanations/contract.txt`;
-const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
-
 export const metadataViews = {
   slug: recipe,
   title: "Metadata Views",
@@ -18,10 +10,6 @@ export const metadataViews = {
     "https://play.onflow.org/41befd2d-31f3-47f0-ae30-aad776961e31?type=script&id=60b68d62-f15e-402e-9048-ef3f27e224af",
   excerpt:
     "This is how you use the metadata contract to actually access the views to show your metadata.",
-  smartContractCode: contractPath,
-  smartContractExplanation: smartContractExplanationPath,
-  transactionCode: transactionPath,
-  transactionExplanation: transactionExplanationPath,
   filters: {
     difficulty: "beginner",
   },
